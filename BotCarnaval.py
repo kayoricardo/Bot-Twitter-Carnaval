b9d5b57b5448bd363eb10456181090085f5eafbb
kayoricardo <kayoricardo1@gmail.com>
2021-07-05 18:47:16 -0300

    Bot twitter Carnaval

---
+++ BotCarnaval.py
@@ -0,0 +1,18 @@
+import tweepy
+import datetime
+consumer_key = ''
+consumer_secret = ''
+
+acess_token = ''
+acess_secret_token = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(acess_token, acess_secret_token)
+api = tweepy.API(auth)
+
+carnaval = datetime.date(2022,2,26)
+hoje = datetime.date.today()
+
+diferenca = carnaval - hoje
+
+api.update_status(f'Faltam {diferenca.days} para o carnaval 2022')
